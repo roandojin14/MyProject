3d7d8a8d3bbd69327ecb9e8eeb1a0dffdc954376
Jin_Lee <ronaldojin14@gmail.com>
2021-09-27 16:17:00 +0900

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-
-int main()
-{
-	pringf("Hello, world!\n");
-
-	return 0;
-}
+import telepot
+token = '2032815249:AAFz8Rz1CBVPYThSZMQeTnprbMqr_HiZY3M'
+mc = '1961623164'
+bot = telepot.Bot(token)
+def handle(msg):
+    mc = msg['from']['id']
+    con = msg['text']
+    if con == '살아있나?':
+        bot.sendMessage(mc, '살아있다')
+bot.message_loop(handle)
+while 1:pass
